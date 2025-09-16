8e8a881dd967f8386086b372b1850c3a565a8065
fadelfffar <fadel.farinsqi1739@gmail.com>
2025-09-16 11:24:51 +0700

    basic listtile with borderradius decoration

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,21 +8,7 @@ class AnswerModel {
   AnswerModel({
     required this.answer,
     required this.isCorrect
-});
-  // Factory constructor for JSON deserialization
-  factory AnswerModel.fromJson(Map<String, dynamic> json) {
-    return AnswerModel(
-      answer: json['answer'] as String,
-      isCorrect: json['is_correct'] as bool,
-    );
-  }
-    // Convert to JSON for database operations
-    Map<String, dynamic> toJson() {
-      return {
-        'answer': answer,
-        'is_correct': isCorrect,
-      };
-    }
+  });
 }
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -32,54 +18,44 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  late final _response = Supabase.instance.client
-      .from('answer1')
-      .select()
-      .asStream();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        padding: EdgeInsets.all(12),
-        child: Column(
-          children: [
-            Text("Please Answer this question, pick the correct box to answer!", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600),),
-            Text("What does 'Ich bin müde' mean in English?", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-            SizedBox(height: 20,),
-            Expanded(
-                child: StreamBuilder<List<Map<String, dynamic>>>(
-                    stream: _response,
-                    builder: (context, snapshot){
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return Text("Loading");
-                      }
-                      if (snapshot.hasData) {
-                        final answers = snapshot.data!;
-                        return ListView.builder(
-                          padding:  EdgeInsets.all(12),
-                          itemCount: answers.length,
-                          itemBuilder: (context, index) {
-                            final answer = answers[index];
-                            return Container(
-                              child: Text(answer['answer']),
-                            );
-                          },
-                        );
-                      }
-                      else if (snapshot.hasError) {
-                        return Text('Something went wrong');
-                      } else if (!snapshot.hasData) {
-                        // By default, show a loading spinner.
-                        return const CircularProgressIndicator();
-                      }
-                      return Text("No comments found");
-                    }
-                    ),
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        title: Text('Row with 2 Columns & ListTiles'),
+        backgroundColor: Colors.blue,
       ),
-    );
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Row(
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        shape: BoxShape.rectangle,
+                        border: Border.all(color: Colors.white, width: 3),
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                        gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [Colors.blue, Colors.green],
+                        ),
+                      ),
+                      child: ListTile(
+                        title: Text("I don't know what to do"),
+                        subtitle: Text("This is subtitle"),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Divider(),
+            ],
+          ),
+      ),
+      );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'home_screen.dart';
 
-Future<void> main() async {
-  await Supabase.initialize(
-    // TODO() : IMPORTANT!!! make this key and url private when committing to github
-    url: 'https://yzujasgkedpvjiwovsic.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6dWphc2drZWRwdmppd292c2ljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc5MTk0MDYsImV4cCI6MjA3MzQ5NTQwNn0.h1fiWKWXfHb3djo2NUaVXeG8nchfC2uUTe7Df7P-5_E',
-  );
+void main() async {;
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const LingoApp());
 }
@@ -23,6 +18,10 @@ class _LingoAppState extends State<LingoApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Row Column ListTile Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: HomeScreen(),
     );
   }
